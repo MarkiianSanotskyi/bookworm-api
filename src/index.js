947e4547017bad66a302b2c0f74b65022e5e9b1a
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-09-28 19:06:33 -0400

    bluebird added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
 import auth from '../routes/auth';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
 app.use('/api/auth', auth);
