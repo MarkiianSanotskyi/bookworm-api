414dba0154acd2a75c6b3302aaad4e400f6853b2
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-09-27 20:13:15 -0400

    Solver problem

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import User from '../models/User';
+import User from './models/User';
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
     const { credentials } = req.body;
-    User.finOne({ email: credentials.email }).then(user => {
+    User.findOne({ email: credentials.email }).then(user => {
         if (user) {
-
+            
         } else {
             res.status(400).json({ errors: { global: "Invalid credentials"}});
         }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import auth from '../routes/auth';
 
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect('mongo://localhost/bookwarm', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/bookwarm', { useMongoClient: true });
 
 app.use('/api/auth', auth);
 
-app.post("/api/auth", (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentialds "} });
-});
+// app.post("/api/auth", (req, res) => {
+//     res.status(400).json({ errors: { global: "Invalid credentialds "} });
+// });
 
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 app.listen(8080, () => console.log("Running on localhost:8080"));
