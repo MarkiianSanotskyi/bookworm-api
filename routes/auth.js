172b249fa866fd21ba10c0edc46afe5933e6bfaf
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-08 21:08:39 -0400

    Began work with Email Confirmation

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,16 @@ import User from './models/User';
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
     const { credentials } = req.body;
     User.findOne({ email: credentials.email }).then(user => {
-        if (user && user.isValidPassword(credentials.password)) {
-            res.json({ user: user.toAuthJSON() });
-        } else {
-            res.status(400).json({ errors: { global: "Invalid credentials"}});
-        }
+      if (user && user.isValidPassword(credentials.password)) {
+        res.json({ user: user.toAuthJSON() });
+      } else {
+        res.status(400).json({ errors: { global: "Invalid credentials" } });
+      }
     });
-});
+  });
 
 
 export default router;
--- routes/models/User.js
+++ routes/models/User.js
@@ -14,7 +14,8 @@ const schema = new mongoose.Schema({
         unique: true
       },
       passwordHash: { type: String, required: true },
-      confirmed: { type: Boolean, default: false }
+      confirmed: { type: Boolean, default: false },
+      confirmationToken: { type: String, default: "" }
 },
  { timestamps: true}
 );
@@ -27,20 +28,30 @@ schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrypt.hashSync(password, 10);
   };
 
-schema.methods.generateJWT = function generateJWT() {
-    return jwt.sign({
-        email: this.email
-    }, process.env.JWT_SECRET);
-};
+  schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+  };
+  
+  schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+  };
+  
 
-schema.methods.toAuthJSON = function toAuthJSON() {
-    return {
+  schema.methods.generateJWT = function generateJWT() {
+    return jwt.sign(
+      {
         email: this.email,
-        confirmed: this.confirmed,
-        token: this.generateJWT()
+        confirmed: this.confirmed
+      },
+      process.env.JWT_SECRET
+    );
+  };
+  schema.methods.toAuthJSON = function toAuthJSON() {
+    return {
+      email: this.email,
+      confirmed: this.confirmed,
+      token: this.generateJWT()
     };
-};
-
-schema.plugin(uniqueValidator, { message: 'This email is already taken' });
-
-export default mongoose.model('User', schema);
+  };
+  schema.plugin(uniqueValidator, { message: "This email is already taken" });
+  export default mongoose.model("User", schema);
