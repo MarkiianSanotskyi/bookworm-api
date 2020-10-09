05709786b82ce31f05999a4e2e09b355489d4f51
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-08 20:31:02 -0400

    Added function of validation an Email Uniqueness

--- routes/models/User.js
+++ routes/models/User.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 
 // TODO: add uniqueness and email validations to email field
 const schema = new mongoose.Schema({
-    email: { type: String, required: true, lowercase: true, index: true},
-    passwordHash: { type: String, required: true}
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        index: true,
+        unique: true
+      },
+      passwordHash: { type: String, required: true },
+      confirmed: { type: Boolean, default: false }
 },
  { timestamps: true}
 );
@@ -14,6 +22,10 @@ const schema = new mongoose.Schema({
 schema.methods.isValidPassword = function isValidPassword(password) {
     return bcrypt.compareSync(password, this.passwordHash);
 };
+
+schema.methods.setPassword = function setPassword(password) {
+    this.passwordHash = bcrypt.hashSync(password, 10);
+  };
 
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
@@ -24,8 +36,11 @@ schema.methods.generateJWT = function generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
     return {
         email: this.email,
+        confirmed: this.confirmed,
         token: this.generateJWT()
     };
 };
+
+schema.plugin(uniqueValidator, { message: 'This email is already taken' });
 
 export default mongoose.model('User', schema);
